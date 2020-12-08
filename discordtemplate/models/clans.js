a48bc9d9b47d5575ee4c9624dcbdd9ad625e6141
AlienTina <tomas122.tb@gmail.com>
2020-12-08 23:21:46 +0100

    Removed useless junk

--- discordtemplate/models/clans.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-var clans = new Schema({
-    _version: Number,
-    clanID: Number,
-    clanOwner: String
-})
-module.exports = model('clans', clans, 'clans')
--- discordtemplate/models/srv.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-var srv = new Schema({
-    _version: Number,
-    guildID: String,
-    customPrefix: String
-});
-module.exports = model('srv', srv, 'srv');
